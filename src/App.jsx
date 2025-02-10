095dd3d45cba04b380ce95122e8d52cae2b1fe96
jeganathan I <jegan4044@gmail.com>
2025-02-10 05:37:51 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Square from './Square.jsx'
+import Input from './Input.jsx'
+import {useState} from 'react'
+const App = () => {
+    const [colorValue, setColorValue] = useState('')
+    const [hexValue, setHexValue] = useState('')
+    const [isDarkText, setIsDarkText ] = useState(true)
+    return (
+        <div className="App">
+            <Square
+                colorValue={colorValue}
+                hexValue={hexValue}
+                isDarkText={isDarkText}/>
+            <Input
+            colorValue={colorValue}
+            setColorValue={setColorValue}
+            setHexValue={setHexValue}
+            isDarkText={isDarkText}
+            setIsDarkText={setIsDarkText}
+            />
+        </div>
+    )
+}
+export default App
---
+++ src/Square.jsx
@@ -0,0 +1,15 @@
+const Square = ({ colorValue, hexValue, isDarkText}) => {
+    return (
+        <section className="square"
+        style={{ backgroundColor: colorValue ,
+        color: isDarkText ? "#000" : "#FFF"
+        }}>
+            <p>{colorValue ? colorValue : "Empty Value"}</p>
+            <p>{hexValue ? hexValue : null}</p>
+        </section>
+    )
+}
+Square.defaultprops ={
+    colorValue: "Empty Color Value"
+}
+export default Square
